e207ad9de8dd61384a4e861954c2b029daa13ae1
Nils VAN ZUIJLEN <nils.van-zuijlen@mailo.com>
2019-05-02 16:33:33 +0200

    First with pinout

---
+++ pinout.h
@@ -0,0 +1,29 @@
+#ifndef PINOUT_H
+#define PINOUT_H 1
+
+#define DEBUG_LED 13
+
+#define MOTOR_CH_A 0
+#define MOTOR_CH_B 1
+#define MOTOR_BRAKE 2
+#define MOTOR_DIR 3
+#define MOTOR_PWM 4
+#define MOTOR_THERM 5
+
+#define PUMP_BRAKE 28
+#define PUMP_DIR 29
+#define PUMP_PWM 30
+#define PUMP_THERM 31
+
+#define SERVO_1 38
+#define SERVO_2 37
+
+#define STEPPER_FAULT 36
+#define STEPPER_SLEEP 35
+#define STEPPER_STEP 34
+#define STEPPER_DIR 33
+
+#define END_STOP_T 14  // translation
+#define END_STOP_R 15  // rotation
+
+#endif
