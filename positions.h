18077d40b0dd27b959800c450c7c7df6f1e452c6
Nils VAN ZUIJLEN <nils.van-zuijlen@mailo.com>
2019-05-09 15:28:12 +0200

    Add position macros.

---
+++ positions.h
@@ -0,0 +1,37 @@
+#ifndef POSITIONS_H
+#define POSITIONS_H 1
+
+namespace positions {
+
+namespace angle { // servo angles 
+	const int HORIZONTAL  = 0;
+	const int VERTICAL    = 0;
+	const int ANGLED_BACK = 0;
+	const int STORAGE     = 0;
+}
+
+namespace rotation { // arm rotation
+	const int FRONT = 0;
+	const int RIGHT = 0;
+	const int LEFT  = 0;
+	const int STORE_1 = 0;
+	const int STORE_2 = 0;
+}
+
+namespace height {
+	const int BOTTOM = 0;
+	const int TOP = 0;
+
+	const int PALET_BACK = 0;
+	const int PALET_FRONT = 0;
+	const int GOLDENIUM = 0;
+
+	const int STORE_0 = 0; // height of store places
+	const int STORE_1 = 0;
+	const int STORE_2 = 0;
+	const int STORE_3 = 0;
+}
+
+} // namespace positions
+
+#endif
